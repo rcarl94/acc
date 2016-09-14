7f3cc5a1ac2412dba810b461ce1fcaf07852c34c
Ryan <rcarl94@gmail.com>
2016-09-14 07:47:14 -0500

    Fixed small screen nav

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
   $("#menu-toggle").click(function() {
-    $("#menu-toggle").toggle();
-    $("#nav").toggle();
+    if ($("#menu-toggle i:first-child").css("display") == "inline-block") {
+      $("#menu-toggle i:first-child").fadeOut(function() {
+        $("#menu-toggle i:nth-child(2)").fadeIn();
+      });
+      $("#nav").css("height","100%");
+      $("#nav").css("width","100%");
+      $(".navBtnContainer").css("display","block");
+      $(".navBtnContainer").css("display","block");
+    } else {
+      $("#menu-toggle i:nth-child(2)").fadeOut(function() {
+        $("#menu-toggle i:first-child").fadeIn();
+      });
+      $(".navBtnContainer").hide();
+      $("#nav").css("height","auto");
+      $("#nav").css("width","auto");
+    }
   });
 });
