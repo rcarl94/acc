76f1378018499978b02f95d2ed8e96789aa4736a
rcarl94 <rcarl94@gmail.com>
2016-12-24 05:34:09 +0000

    request loading

--- js/app.js
+++ js/app.js
@@ -36,4 +36,49 @@ $(document).ready(function() {
     afterClose: function() {
     }
   });
+
+  function showUpcoming() {
+    loadCalendarApi();
+  }
+
+  /**
+   *        * Load Google Calendar client library. List upcoming events
+   *               * once client library is loaded.
+   *                      */
+  function loadCalendarApi() {
+    gapi.client.load('calendar', 'v3', listUpcomingEvents);
+  }
+
+  /**
+*        * Print the summary and start datetime/date of the next ten events in
+*               * the authorized user's calendar. If no events are found an
+*                      * appropriate message is printed.
+*                             */
+  function listUpcomingEvents() {
+    var request = gapi.client.calendar.events.list({
+      'calendarId': 'destincondocalendar@outlook.com',
+      'timeMin': (new Date()).toISOString(),
+      'showDeleted': false,
+      'singleEvents': true,
+      'orderBy': 'startTime'
+    });
+
+    request.execute(function(resp) {
+      var events = resp.items;
+
+      if (events.length > 0) {
+        for (i = 0; i < events.length; i++) {
+          var event = events[i];
+          var when = event.start.dateTime;
+          if (!when) {
+            when = event.start.date;
+          }
+          $("#requests").append("<div class='request'><span>Name</span>" + event.summary + "<br><span>Dates</span>" + event.start.date + " - " + event.end.date + "<br><span>Additional Info</span>" + event.description + "</div>");
+        }
+      } else {
+        $("#requests").append('No upcoming events found.');
+      }
+
+    });
+  }
 });
